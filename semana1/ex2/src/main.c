d97b87cef6af4887b4c0f7548987019a835d9e51
mthsrnn <matheusfreitas.pl070@academico.ifsul.edu.br>
2024-04-21 18:25:03 -0300

    ex2 feito em casa - sem usar lista encadeada

--- semana1/ex2/src/main.c
+++ semana1/ex2/src/main.c
@@ -1,79 +1,144 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define INI_ENTRADA 10
 
 void imprimeMenu(void);
-void limpaStdin(void);
-int adicionaNome(char * entrada, char ** saida);
-char *removeNome(char *entrada);
+char *scanEntrada(void);
+char *adicionaNome(char *entrada, char *vetor);
+char *removeNome(char *entrada, char *vetor);
 void listaNomes(char *entrada);
 
 int main(void) {
-  char opt, buffer;
-  char *lista = (char *) calloc(1, sizeof(char));
-  char *entrada;
-  int quantosNomes = 0;
-  int *indiceAdicionados;
+  int opt;
+  char *lista = (char *)calloc(1, sizeof(char));
+  char *entrada, c;
 
   do {
     imprimeMenu();
 
-    scanf("%c^['\n']", &opt);
+    scanf("%d", &opt);
 
     switch (opt) {
-      case '1':
-        puts("Insirar o nome para adicionar:");
-        scanf("%s", entrada);
-        indiceAdicionados = (int *) realloc(indiceAdicionados, sizeof(int)*(quantosNomes+1));
-        indiceAdicionados[quantosNomes] = adicionaNome(entrada, &lista);
-        quantosNomes++;
+    case 1:
+      puts("Insirar o nome para adicionar:");
+      entrada = scanEntrada();
+      lista = adicionaNome(entrada, lista);
+      free(entrada);
       break;
 
-      case '2':
-        puts("Insirar o nome para remover:");
-        scanf("%s", entrada);
-        //lista = removeNome(entrada);
+    case 2:
+      puts("Insirar o nome para remover:");
+      entrada = scanEntrada();
+      lista = removeNome(entrada, lista);
       break;
 
-      case '3':
-        //listaNomes(entrada);
-        puts(lista);
+    case 3:
+      // listaNomes(entrada);
+      puts(lista);
       break;
 
-      case '4':
-        return EXIT_SUCCESS;
+    case 4:
+      return EXIT_SUCCESS;
       break;
 
-      default:
-        puts("Opçao invalida!");
+    default:
+      puts("Opçao invalida!");
       break;
-
     }
-    
-  } while (opt != '4');
-  
+
+  } while (opt != 4);
+  if(entrada != NULL)
+    free(entrada);
+
+  free(lista);
+
   return EXIT_SUCCESS;
 }
 
-void imprimeMenu(void){
+void imprimeMenu(void) {
   puts("Escolha a operação:\n1 - Adiciona nome\n2 - Remove nome\n3 - Lista os nomes\n4 - Sair");
 }
 
-int adicionaNome(char * entrada, char ** saida){
-  int lastpos = strlen(*saida);
-  int oldpos = lastpos;
-  char * novoPonteiro = realloc(*saida, sizeof(char)*(strlen(*saida)+strlen(entrada)-1));
-  
-  if (novoPonteiro== NULL){
-    fputs("Erro de alocacao", stderr);
+char *scanEntrada(void) {
+  size_t tamanho = INI_ENTRADA;
+  char *saida = malloc(sizeof(char) * INI_ENTRADA);
+  if (saida == NULL) {
+    fputs("Erro de alocacao.", stderr);
+    exit(EXIT_FAILURE);
+  }
+  int c;
+  int i = 0;
+  while ((c = getchar()) != '\n' && c != EOF); //limpa o buffer
+  while ((c = getchar()) != '\n' && c != EOF) {
+    if (i == tamanho) {
+      tamanho *= 2;
+      saida = realloc(saida, sizeof(char) * tamanho);
+      if (saida == NULL) {
+        fputs("Erro de alocacao.", stderr);
+        exit(EXIT_FAILURE);
+      }
+    }
+    saida[i] = c;
+    i++;
+  }
+  saida = realloc(saida, sizeof(char) * (i));
+  if (saida == NULL) {
+    fputs("Erro de alocacao.", stderr);
+    exit(EXIT_FAILURE);
+  }
+  saida[i] = '\0';
+  return saida;
+}
+
+char *adicionaNome(char *entrada, char *vetor) {
+  int tamanhoEntrada = strlen(entrada), i;
+  int tamanhoVetor = strlen(vetor);
+  char *novoPonteiro = realloc(vetor, sizeof(char) * (tamanhoVetor + tamanhoEntrada + 2)); //o "padding" (+2) extra é pra caber o \0 e a virgula
+
+  if (novoPonteiro == NULL) {
+    fputs("Não foi possivel realocar o vetor", stderr);
     exit(EXIT_FAILURE);
   }
 
-  for( int i = 0; i < strlen(entrada); i++ ){
-    novoPonteiro[lastpos] = entrada[i];
-    lastpos ++;
+  for (i = 0; i < tamanhoEntrada; i++) {
+    novoPonteiro[tamanhoVetor++] = entrada[i];
   }
 
-  *saida = novoPonteiro;
-  return oldpos;
+  novoPonteiro[tamanhoVetor++] = ',';
+
+  novoPonteiro[tamanhoVetor]='\0';
+
+  return novoPonteiro;
+}
+
+char *removeNome(char *entrada, char *vetor) {
+  int tamanhoEntrada = strlen(entrada), i, j, k;
+  int tamanhoVetor = strlen(vetor);
+
+  if (tamanhoEntrada <= 0 || tamanhoVetor <= 0) {
+    puts("Entrada e/ou saida vazias(s)");
+    return vetor;
+  }
+  
+  for (i = 0; i < tamanhoVetor; i++){
+    if (vetor[i] == entrada[0]){
+      for (j = 0; j < tamanhoEntrada; j++){
+        if(vetor[i + j] != entrada[j])
+          break;
+      }
+      if (j == tamanhoEntrada && (i == 0 && (vetor[j] == ',') || (vetor[i - 1] == ',') && (vetor[i+j]==','))){
+        tamanhoVetor -= tamanhoEntrada;
+        while (i < tamanhoVetor){
+          vetor[i] = vetor[i+j+1];
+          i++;
+        }
+        vetor[tamanhoVetor] = '\0';
+        return realloc(vetor, sizeof(char)*tamanhoVetor);
+      }
+    }
+  }
+
+  puts("string nao encontrada.");
+  return vetor;
 }
